71ca0fef6763475bfc1968313917cdd804b48ebd
Diego Andres Carvajal <diegocarvajalruiz@outlook.com>
2016-05-26 21:11:04 -0500

    Cambio 01

---
+++ HelloWorld/src/App.java
@@ -0,0 +1,16 @@
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Que bonito son los Parciales de Cálculo Integral..! XD");
+		
+		int n1 = 2;
+		int n2 = 7;
+		
+		System.out.println(n1);
+		System.out.println(n2);
+
+	}
+
+}
