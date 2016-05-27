f013717fffaca1be2c32401771111fc3aeec9f82
dacru-lcms <dacru-lcms@dacrulcms-HP-ProBook-440-G3>
2016-05-27 08:43:42 -0500

    Cambio 03

--- HelloWorld/src/App.java
+++ HelloWorld/src/App.java
@@ -1,15 +1,17 @@
-
 public class App {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Que bonito son los Parciales de Cálculo Integral..! XD");
+		System.out.println("Que bonito son los Parciales de Cï¿½lculo Integral..! XD");
 		
 		int n1 = 65;
 		int n2 = 45;
+		int suma = n1+n2;
 		
 		System.out.println(n1);
 		System.out.println(n2);
+		
+		System.out.println(suma);
 
 	}
 
